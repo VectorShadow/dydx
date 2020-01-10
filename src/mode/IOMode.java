fa0fd103f33873ceb54a40d763afb957d265a2be
VectorShadow <vectorshadowdigitallabs@gmail.com>
2020-01-11 02:36:12 +0430

    Expanded InputDialog support and functionality.

--- src/mode/IOMode.java
+++ src/mode/IOMode.java
@@ -1,6 +1,6 @@
 package mode;
 
-import contract.menu.Menu;
+import contract.input.InputDialog;
 import contract.menu.MenuHandler;
 
 public class IOMode {
@@ -8,15 +8,23 @@ public class IOMode {
 
     private final int channelIndex;
     private final InputContext inputContext;
+    private final InputDialog inputDialog;
     private final MenuHandler menuHandler;
     private long uid;
 
     public IOMode(int channel, InputContext context) {
-        this(channel, context, null);
+        this(channel, context, null, null);
+    }
+    public IOMode(int channel, InputContext context, InputDialog dialog) {
+        this(channel, context, dialog, null);
     }
     public IOMode(int channel, InputContext context, MenuHandler handler) {
+        this(channel, context, null, handler);
+    }
+    public IOMode(int channel, InputContext context, InputDialog dialog, MenuHandler handler) {
         channelIndex = channel;
         inputContext = context;
+        inputDialog = dialog;
         menuHandler = handler;
         uid = uniqueSerialID++;
     }
@@ -27,6 +35,10 @@ public class IOMode {
 
     public InputContext getInputContext() {
         return inputContext;
+    }
+
+    public InputDialog getInputDialog() {
+        return inputDialog;
     }
 
     public MenuHandler getMenuHandler() {
