87f33b9d96582b4b395433ad9384d1cfe6938178
VectorShadow <vectorshadowdigitallabs@gmail.com>
2020-01-17 07:07:36 +0430

    Implemented Character Class, Class Selection Menu.
    Added menu shortcut hotkey display and input handling.

--- src/mode/InputContext.java
+++ src/mode/InputContext.java
@@ -1,19 +1,5 @@
 package mode;
 
-import contract.menu.MenuHandler;
-
 public interface InputContext {
-//    public static long uniqueSerialID = 0;
-//
-//    private long uid;
-//
-//    public InputContext() {
-//        uid = uniqueSerialID++;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        return o instanceof InputContext && ((InputContext) o).uid == uid;
-//    }
     void handle(int keyCode, int keyModifiers, IOMode mode);
 }
