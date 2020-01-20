b918464ea01908a3418a530048b6b88444b13e1f
VectorShadow <vectorshadowdigitallabs@gmail.com>
2020-01-20 22:31:31 +0430

    Implemented ACEThemeLookupTable.
    Adjusted hacks to test.

--- src/mapgen/FloorDesigner.java
+++ src/mapgen/FloorDesigner.java
@@ -10,7 +10,7 @@ public class FloorDesigner {
         //todo - MEGAHACK - use the theme lookup table to find a theme from the world coordinate.
         // Then use this theme to pick a MapGenerator and build the level from it.
         // Note that Level constructor needs to be completely refactored before this can be done properly.
-        return new Level(CoreProcesses.isRealTime(), 32, 32, 0);
+        return new Level(CoreProcesses.isRealTime(), 36, 36, 1);
     }
 
 }
